c0a427abf24326d5cd280f8eeac83d9d85b2e9da
SLuvizotto <luvizottodev@gmail.com>
2025-07-26 00:47:35 -0300

    feat: adiciona automação para abrir o YouTube e buscar "lofi girl"

--- automacao.py
+++ automacao.py
@@ -3,5 +3,11 @@ import time
 pa.PAUSE = 1 # espera 1 seg. entre OPs
 
 pa.press('win') # press -> pressionar tecla 
-pa.write('chrome') # write -> escrever
+pa.write("chrome") # write -> escrever
+pa.press('ENTER')
+pa.write("youtube.com")
+pa.press('ENTER')
+time.sleep(2)
+pa.click(x=582, y=114)
+pa.write("lofi girl")
 pa.press('ENTER')
