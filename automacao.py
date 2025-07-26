620e4605f958905a731850dbb204261c25538d8f
SLuvizotto <luvizottodev@gmail.com>
2025-07-26 00:38:53 -0300

    adiciona script de automação para abrir o Chrome

---
+++ automacao.py
@@ -0,0 +1,7 @@
+import pyautogui as pa
+import time
+pa.PAUSE = 1 # espera 1 seg. entre OPs
+
+pa.press('win') # press -> pressionar tecla 
+pa.write('chrome') # write -> escrever
+pa.press('ENTER')
