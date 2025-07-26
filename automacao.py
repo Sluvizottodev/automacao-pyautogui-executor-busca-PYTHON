43cb47c6c746f90ab8405cf086e933abaad54e78
SLuvizotto <luvizottodev@gmail.com>
2025-07-26 00:56:54 -0300

    feat: adiciona suporte para copiar texto com acento e espera após a busca no YouTube

--- automacao.py
+++ automacao.py
@@ -1,5 +1,6 @@
 import pyautogui as pa
 import time
+import pyperclip # para palavras com acento
 pa.PAUSE = 1 # espera 1 seg. entre OPs
 
 pa.press('win') # press -> pressionar tecla 
@@ -11,3 +12,10 @@ time.sleep(2)
 pa.click(x=582, y=114)
 pa.write("lofi girl")
 pa.press('ENTER')
+time.sleep(2)
+pa.click(x=512, y=501)
+
+
+#pyperclip.copy("legião urbana")
+#pa.hotkey('ctrl', 'v')
+#pa.press('ENTER')
