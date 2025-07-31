03e207b0b579282fa4aa121b570364e1136bb17d
SLuvizotto <luvizottodev@gmail.com>
2025-07-31 00:44:00 -0300

    feat: altera confirmação ao esvaziar a lixeira para True

--- limpar_lixeira.py
+++ limpar_lixeira.py
@@ -1,7 +1,7 @@
 import winshell
 
 try:
-    winshell.recycle_bin().empty(confirm=False, show_progress=False, sound=False)
+    winshell.recycle_bin().empty(confirm=True, show_progress=False, sound=False)
     print("Lixeira esvaziada com sucesso!")
 except Exception as e:
     print("A lixeira já estava vazia ou houve erro:", e)
