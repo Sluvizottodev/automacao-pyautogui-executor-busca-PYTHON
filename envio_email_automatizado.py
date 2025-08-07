2bed387ff4373916fac872c13d60cdf78f9a2032
SLuvizotto <luvizottodev@gmail.com>
2025-08-06 23:52:56 -0300

    fix: corrige erro de digitação na importação do módulo smtplib

--- envio_email_automatizado.py
+++ envio_email_automatizado.py
@@ -1,10 +1,13 @@
-import stmplib
+import smtplib
 from email.mime.text import MIMEText
 import os
+from dotenv import load_dotenv  # <-- aqui
+
+load_dotenv()  # carrega as variáveis do .env
 
 rem = 'luvizottodev@gmail.com'
 senha = os.getenv('EMAIL_APP_PASSWORD')
-
+print('Senha obtida:', senha)  
 dest = 'stefani292005@gmail.com'
 
 msg = MIMEText('Teste de envio de email automatizado')
@@ -12,7 +15,7 @@ msg['Subject'] = 'Email automatizado'
 msg['From'] = rem
 msg['To'] = dest   
 
-with stmplib.SMTP('smtp.gmail.com', 587) as server:
+with smtplib.SMTP('smtp.gmail.com', 587) as server:
     server.starttls()
     server.login(rem, senha)
     server.sendmail(rem, dest, msg.as_string())
