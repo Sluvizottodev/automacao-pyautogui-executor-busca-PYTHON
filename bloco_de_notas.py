46688e86097a49798266df1a17811489a92c47ff
SLuvizotto <luvizottodev@gmail.com>
2025-08-08 23:10:29 -0300

    feat: melhora a inserção de texto no bloco de notas utilizando pyperclip

--- bloco_de_notas.py
+++ bloco_de_notas.py
@@ -1,11 +1,14 @@
+# -*- coding: utf-8 -*-
 import pyautogui
 import time
 import os
+import pyperclip
 
 os.system("Start notepad")
 time.sleep(1)
 
-pyautogui.write("Olá, mundo!", interval=0.05)
+pyperclip.copy("Olá, mundo!")
+pyautogui.hotkey("ctrl", "v")
 time.sleep(0.5)
 
 pyautogui.hotkey("ctrl", "s")
