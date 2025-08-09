f56846f7fc03abbb9ff45aa1297dab7897a50dea
SLuvizotto <luvizottodev@gmail.com>
2025-08-08 23:05:51 -0300

    feat: adiciona funcionalidade para abrir o bloco de notas e salvar um arquivo com texto

--- bloco_de_notas.py
+++ bloco_de_notas.py
@@ -1,3 +1,15 @@
 import pyautogui
 import time
 import os
+
+os.system("Start bloco de notas")
+time.sleep(1)
+
+pyautogui.write("Olá, mundo!", interval=0.05)
+time.sleep(0.5)
+
+pyautogui.hotkey("ctrl", "s")
+time.sleep(0.5)
+
+pyautogui.write("ex_automacao_notas_py.txt")
+pyautogui.press("enter")
