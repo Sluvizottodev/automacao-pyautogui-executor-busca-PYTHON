2d9430b2e3dbeed6a6b8220353c4e56a30ad0fcc
SLuvizotto <luvizottodev@gmail.com>
2025-08-18 22:30:36 -0300

    refactor: melhora a legibilidade dos comentários no script de análise gráfica

--- analise_grafica.py
+++ analise_grafica.py
@@ -5,16 +5,18 @@ x = np.linspace(0,2,100)
 
 fig, ax = plt.subplots()
 
-
+# Função linear (y = x)
 ax.plot(x, x, label='linear')
 
+# Função quadrática (y = x²)
 ax.plot(x, x**2, label='quadratic')
 
+# Função cúbica (y = x³)
 ax.plot(x, x**3, label='cubic')
 
-ax.set_xlabel('x label')
-ax.set_ylabel('y label')
-ax.set_title('Simple Plot')
-ax.legend()
+ax.set_xlabel('x label') # apelido do eixo das abscissas (x)
+ax.set_ylabel('y label') # apelido do eixo das ordenadas (y)
+ax.set_title('Simple Plot') # titulo graf.
+ax.legend() # exibe legenda
 
-plt.show()
+plt.show() # exibe grafico 
